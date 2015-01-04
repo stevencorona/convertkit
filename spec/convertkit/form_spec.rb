6cc5da857226dace8763c553f73d23dcbb3793ff
Steve Corona <thestevecorona@gmail.com>
2015-01-03 19:30:12 -0800

    write specs for loading all forms

--- spec/convertkit/form_spec.rb
+++ spec/convertkit/form_spec.rb
@@ -2,5 +2,23 @@ require "spec_helper"
 
 module ConvertKit
   describe Form do
+  	let(:client) { ConvertKit::Client.new("f9922361d11e7339cc1cde3d54c071") }
+
+  	it "returns a array of form objects" do
+
+  		forms = client.forms()
+
+  		expect(forms).to be_a(Array)
+  		expect(forms.count).to eq(1)
+  		expect(forms.first).to be_a(ConvertKit::Form)
+
+  		# Check the data is set
+  		form = forms.first
+  		expect(form.id).to eq(4812649)
+  		expect(form.subscriber_count).to eq(0)
+  		expect(form.name).to eq("Test form")
+
+  	end
+
   end
 end
